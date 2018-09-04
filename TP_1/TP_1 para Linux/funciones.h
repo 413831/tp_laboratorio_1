70f6854d8922ddc0aee3616063a286f3dd314287
413831 <40808636+413831@users.noreply.github.com>
2018-09-04 12:20:52 -0300

    Add files via upload

---
+++ TP_1/TP_1 para Linux/funciones.h
@@ -0,0 +1,9 @@
+int getCaracter(char * caracterAuxiliar,char * mensaje);
+int getNumero(float * numeroAuxiliar);
+float funcionSuma(float auxiliarUno,float auxiliarDos);
+float funcionResta(float auxiliarUno,float auxiliarDos);
+float funcionMultiplicar(float auxiliarUno,float auxiliarDos);
+float funcionDividir(float auxiliarUno,float auxiliarDos);
+double funcionFactorial(float auxiliarUno);
+int funcionCalculos(float auxiliarUno,float auxiliarDos);
+int menuCalculosDosNumeros();
